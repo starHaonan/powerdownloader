caa797b760e125092fcbaf69c5c70c82acac0d13
holden <starhaonan@outlook.com>
2025-03-20 22:10:21 +0800

    Initial commit

---
+++ src/main/java/org/example/powerdownloader/PowerDownloaderApplication.java
@@ -0,0 +1,13 @@
+package org.example.powerdownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerDownloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerDownloaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/powerdownloader/PowerDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.powerdownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerDownloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
